6834ddbf7c29fd4ef30c23b64b89b87966a0b06c
Felix Lee <leeyoongti@gmail.com>
2019-10-13 20:37:42 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Profile',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[100],
+        cursorColor: Colors.white,
+        fontFamily: 'Nunito',
+        appBarTheme: AppBarTheme(
+          color: Colors.transparent,
+          iconTheme: IconThemeData(color: Colors.black),
+          elevation: 2,
+        ),
+      ),
+      initialRoute: '/',
+    );
+  }
+}
